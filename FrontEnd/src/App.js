ac4969d94a54e6445b70259a04b7dcd6a7ecd4ba
DongIkkk <w8h0412@gmail.com>
2023-09-12 14:54:09 +0900

    Feat: 프론트 초기 구조 설정

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,7 +1,14 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import Main from './components/main';
-import {Routes,Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import LandingPage from './components/Auth/LandingPage';
+import HomePage from './components/Home/HomePage';
+import BorrowPage from './components/Loan/Borrow/BorrowPage';
+import LendPage from './components/Loan/Lend/LendPage';
+import HistoryPage from './components/Loan/History/HistoryPage';
+import FriendsPage from './components/Friends/FriendsPage';
+import ProfilePage from './components/Profile/ProfilePage';
 
 function App() {
   // vw, vh를 보이는 화면의 %로 계산하는 식
@@ -9,15 +16,30 @@ function App() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   }
-  // useEffect로 실행될 때마다 작동 되게
-  useEffect(()=>{
-    setScreenSize();
-  },[])
   
+  // useEffect로 실행될 때마다 작동 되게
+  useEffect(() => {
+    setScreenSize();
+  }, [])
+
+  const is_autication = false
+
   return (
     <div className="App">
       <Routes> {/*라우터 작동 정리*/}
-        <Route path="*" element={<Main />}></Route>
+        ({is_autication
+          ?
+          <Route path="/" element={<LandingPage />}/>
+          :
+          <>
+            <Route path="/" element={<HomePage />}/>
+            <Route path="/borrow" element={<BorrowPage />}/>
+            <Route path="/lend" element={<LendPage />}/>
+            <Route path="/history" element={<HistoryPage />}/>
+            <Route path="/friends" element={<FriendsPage />}/>
+            <Route path="/profile" element={<ProfilePage />}/>
+          </>
+        })
       </Routes>
     </div>
   );
