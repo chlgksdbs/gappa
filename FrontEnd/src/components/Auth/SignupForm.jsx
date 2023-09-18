cf707c5d0821cf95a3b654e328f7193090e8ac7e
KyongBeom <dragontig98@naver.com>
2023-09-18 13:02:19 +0900

    Feat: signup UI 제작

--- FrontEnd/src/components/Auth/SignupForm.jsx
+++ FrontEnd/src/components/Auth/SignupForm.jsx
@@ -4,39 +4,38 @@ import style from './SignupPage.module.css';
 const SignupForm = () => {
   return (
     <div className={style.form}>
-      <div>
+      <div className={style.idform}>
         <span>아이디</span>
         <br />
-        <input type="text" />
-        <input type="button" value="중복확인"/>
+        <input type="text" className={style.forminput}/>
+        <input type="button" value="중복확인" className={style.formbtn}/>
       </div>
       <>
         <span>이름</span>
-        <input type="text" />
+        <input type="text" className={style.input} />
       </>
       <>
         <span>통신사</span>
-        <input list="phone" name="" id="" />
-        <datalist id="phone">
-          <option value="SKT" />
-          <option value="LG U+" />
-          <option value="KT" />
-          <option value="알뜰폰" />
-        </datalist>
+        <select className={style.input}>
+          <option value="SKT">SKT</option>
+          <option value="KT">KT</option>
+          <option value="LG U+">LG U+</option>
+          <option value="알뜰폰">알뜰폰</option>
+        </select>
       </>
-      <div>
+      <div className={style.phoneform}>
         <span>휴대폰 번호</span>
         <br />
-        <input type="text" />
-        <input type="button" value="인증번호 발송" />
+        <input type="text" className={style.forminput}/>
+        <input type="button" value="인증번호 발송" className={style.formbtn}/>
       </div>
       <>
         <span>비밀번호</span>
-        <input type="text" />
+        <input type="text" className={style.input} />
       </>
       <>
         비밀번호 확인
-        <input type="text" />
+        <input type="text" className={style.input} />
       </>
     </div>
   );
