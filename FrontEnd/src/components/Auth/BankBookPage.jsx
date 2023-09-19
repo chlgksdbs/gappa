fadfc611242cd84a8e5b40bab08ea59cde653051
KyongBeom <dragontig98@naver.com>
2023-09-19 17:41:15 +0900

    Feat : 회원가입 로직 추ㄱ

---
+++ FrontEnd/src/components/Auth/BankBookPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BankBookPage = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default BankBookPage;
--- FrontEnd/src/components/Auth/SignupForm.jsx
+++ FrontEnd/src/components/Auth/SignupForm.jsx
@@ -4,13 +4,87 @@ import DaumPostcode from 'react-daum-postcode';
 import Modal from 'react-modal';
 
 const SignupForm = () => {
-  const [openPostcode, setOpenPostcode] = useState(false);
+
+  //초기값 세팅
   const [address, setAddress] = useState("");
   const [addressNumber, setAddressNumber] = useState("");
   const [phone, setPhone] = useState("");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
 
-  // const [password,setPassword] = useState("");
-  // const [passwordCheck,setPasswordCheck] = useState("");
+  //오류 메세지
+  const [idMessage, setIdMessage] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState("");
+
+  //유효성 검사
+  const [openPostcode, setOpenPostcode] = useState(false);
+  const [isId, setIsId] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+  const [isPasswordConfirm, setIsPasswordConfirm] = useState(false);
+  const [idOverlap, setIdOverlap] = useState(false);
+
+  const onChangeId = (e) => {
+    const currentId = e.target.value;
+    setId(currentId);
+    const idRegExp = /^[a-zA-z0-9]{4,12}$/;
+
+    if (!idRegExp.test(currentId)) {
+      setIdMessage("4-12사이 대소문자 또는 숫자만 입력해 주세요!");
+      setIsId(false);
+    } else {
+      setIdMessage("사용가능한 아이디 입니다.");
+      setIsId(true);
+    }
+  };
+
+  const onChangeIdOverlap = () => {
+    setTimeout(() => {
+      setIdOverlap(true);
+    }, 3000);
+  }
+
+  const onChangePassword = (e) => {
+    const currentPassword = e.target.value;
+    setPassword(currentPassword);
+    const passwordRegExp =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+    if (!passwordRegExp.test(currentPassword)) {
+      setPasswordMessage(
+        "숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!"
+      );
+      setIsPassword(false);
+      if (currentPassword !== passwordConfirm) {
+        setPasswordConfirmMessage("떼잉~ 비밀번호가 똑같지 않아요!");
+        setIsPasswordConfirm(false);
+      } else {
+        setPasswordConfirmMessage("똑같은 비밀번호를 입력했습니다.");
+        setIsPasswordConfirm(true);
+      }
+    } else {
+      setPasswordMessage("안전한 비밀번호 입니다.");
+      setIsPassword(true);
+      if (currentPassword !== passwordConfirm) {
+        setPasswordConfirmMessage("떼잉~ 비밀번호가 똑같지 않아요!");
+        setIsPasswordConfirm(false);
+      } else {
+        setPasswordConfirmMessage("똑같은 비밀번호를 입력했습니다.");
+        setIsPasswordConfirm(true);
+      }
+    }
+  };
+  const onChangePasswordConfirm = (e) => {
+    const currentPasswordConfirm = e.target.value;
+    setPasswordConfirm(currentPasswordConfirm);
+    if (password !== currentPasswordConfirm) {
+      setPasswordConfirmMessage("떼잉~ 비밀번호가 똑같지 않아요!");
+      setIsPasswordConfirm(false);
+    } else {
+      setPasswordConfirmMessage("똑같은 비밀번호를 입력했습니다.");
+      setIsPasswordConfirm(true);
+    }
+  };
   const phoneRef = useRef();
   const handle = {
     // 버튼 클릭 이벤트
@@ -68,6 +142,7 @@ const SignupForm = () => {
         onChange={autoHypenPhone}
         className={style.forminput}
       />
+
     );
   };
 
@@ -77,8 +152,20 @@ const SignupForm = () => {
         <span>아이디</span>
 
         <br />
-        <input type="text" className={style.forminput} name="ID" />
-        <input type="button" value="중복확인" className={style.formbtn} />
+        <input type="text" className={style.forminput} name="ID" value={id} onChange={onChangeId} />
+        <input type="button" value="중복확인" className={style.formbtn} onClick={onChangeIdOverlap} />
+        <Modal
+          isOpen={idOverlap}
+          onRequestClose={() => setIdOverlap(false)}
+        >
+
+        </Modal>
+        {isId
+          ?
+          <span className={style.colorblue}>{idMessage}</span>
+          :
+          <span className={style.colorred}>{idMessage}</span>
+        }
       </div>
       <>
         <span>이름</span>
@@ -86,11 +173,23 @@ const SignupForm = () => {
       </>
       <>
         <span>비밀번호</span>
-        <input type="password" className={style.input} name="password"/>
+        <input type="password" className={style.input} name="password" value={password} onChange={onChangePassword} />
+        {isPassword
+          ?
+          <span className={style.colorblue}>{passwordMessage}</span>
+          :
+          <span className={style.colorred}>{passwordMessage}</span>
+        }
       </>
       <>
         <span>비밀번호 확인</span>
-        <input type="password" className={style.input}  name="passwordcheck"/>
+        <input type="password" className={style.input} name="passwordcheck" value={passwordConfirm} onChange={onChangePasswordConfirm} />
+        {isPasswordConfirm
+          ?
+          <span className={style.colorblue}>{passwordConfirmMessage}</span>
+          :
+          <span className={style.colorred}>{passwordConfirmMessage}</span>
+        }
       </>
       <div className={style.phoneform}>
         <span>휴대폰 번호</span>
