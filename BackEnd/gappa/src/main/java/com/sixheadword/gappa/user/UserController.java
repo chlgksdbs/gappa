a453a1a079db1b4dad912dfbf44ac13059aad27f
김동현 <gkfdkdlel@naver.com>
2023-09-20 17:51:05 +0900

    feat: 휴대폰 인증 컨트롤러 설명추가

--- BackEnd/gappa/src/main/java/com/sixheadword/gappa/user/UserController.java
+++ BackEnd/gappa/src/main/java/com/sixheadword/gappa/user/UserController.java
@@ -14,11 +14,13 @@ public class UserController {
 
     private final UserService userService;
 
+    // API 1. 휴대폰 인증번호 전송
     @PostMapping("/phone/send")
     public ResponseEntity<?> sendVerificationCode(@RequestBody Map<String, String> request){
         return userService.sendVerificationCode(request);
     }
 
+    // API 2. 휴대폰 인증번호 확인
     @PostMapping("/phone/check")
     public ResponseEntity<?> checkVerificationCode(@RequestBody Map<String, String> request){
         return userService.checkVerificationCode(request);
