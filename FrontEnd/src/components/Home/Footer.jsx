ceaf967fb922dead9f22f9b1a31414b1b0b03dc9
김정훈 <junghun2581@naver.com>
2023-09-20 16:57:16 +0900

    Feat: profile 연결

--- FrontEnd/src/components/Home/Footer.jsx
+++ FrontEnd/src/components/Home/Footer.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import style from './Footer.module.css'
+import { useNavigate } from 'react-router-dom';
 
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={style.footer}>
       <img src="./images/FooterHome.png" alt="" />
       <img src="./images/FooterLoan.png" alt="" />
       <img src="./images/FooterFriends.png" alt="" />
-      <img src="./images/FooterProfile.png" alt="" />
+      <img src="./images/FooterProfile.png" alt=""  onClick={() => { navigate("/profile") }}/>
     </div>
   );
 };
