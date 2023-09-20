9d0d1fd63645943bc634adb7992835d9eecfbce7
김동현 <gkfdkdlel@naver.com>
2023-09-20 14:34:47 +0900

    feat: 휴대폰 인증번호 메세지 전송

---
+++ BackEnd/gappa/src/main/java/com/sixheadword/gappa/user/UserController.java
@@ -0,0 +1,23 @@
+package com.sixheadword.gappa.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/phone/send")
+    public ResponseEntity<?> sendVerificationCode(@RequestBody Map<String, String> request){
+        return userService.sendVerificationCode(request);
+    }
+
+
+}
