ec643c510f399ecbe07afc5445643cfc8241afa0
김동현 <gkfdkdlel@naver.com>
2023-09-21 15:24:31 +0900

    Refactor: UserController import 추가

--- BackEnd/gappa/src/main/java/com/sixheadword/gappa/user/UserController.java
+++ BackEnd/gappa/src/main/java/com/sixheadword/gappa/user/UserController.java
@@ -3,6 +3,7 @@ package com.sixheadword.gappa.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
