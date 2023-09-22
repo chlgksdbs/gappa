5344e223df010d99a755ddde7b76a4574e91c964
조해린 <stella9808@naver.com>
2023-09-22 13:23:29 +0900

    Feat: 대출상세조회 관련 API 구현

--- BackEnd/gappa/src/main/java/com/sixheadword/gappa/loanHistory/repository/LoanHistoryRepositoryImpl.java
+++ BackEnd/gappa/src/main/java/com/sixheadword/gappa/loanHistory/repository/LoanHistoryRepositoryImpl.java
@@ -1,4 +1,11 @@
 package com.sixheadword.gappa.loanHistory.repository;
 
-public class LoanHistoryRepositoryImpl {
+import com.sixheadword.gappa.loanHistory.LoanHistory;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.EntityManager;
+
+@RequiredArgsConstructor
+public class LoanHistoryRepositoryImpl implements LoanHistoryRepositoryCustom{
+
 }
