123ef0357d809e915a8a215b4a1fb52e72f17d8e
chlgksdbs <chlgksdbs98@naver.com>
2023-09-25 15:43:02 +0900

    Feat: AllAccept API 생성

---
+++ BackEnd/gappa/src/main/java/com/sixheadword/gappa/allAccept/AllAcceptController.java
@@ -0,0 +1,27 @@
+package com.sixheadword.gappa.allAccept;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/all")
+public class AllAcceptController {
+
+    @GetMapping("/accept")
+    public ResponseEntity<?> allAccept() {
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = HttpStatus.OK;
+
+        resultMap.put("message", status);
+
+        return new ResponseEntity<>(resultMap, status);
+    }
+}
