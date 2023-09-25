9f4068115487b1aaf30c5fc46766624558274b0a
chlgksdbs <chlgksdbs98@naver.com>
2023-09-25 10:27:02 +0900

    Feat: GappaApplication에 BatchProcessing 어노테이션 추가

--- BackEnd/gappa/src/main/java/com/sixheadword/gappa/GappaApplication.java
+++ BackEnd/gappa/src/main/java/com/sixheadword/gappa/GappaApplication.java
@@ -1,11 +1,12 @@
 package com.sixheadword.gappa;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@EnableBatchProcessing
 @SpringBootApplication
-//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class GappaApplication {
 
 	public static void main(String[] args) {
