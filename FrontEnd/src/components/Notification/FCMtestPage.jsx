f5d77f46ba3c2247ed25ca149353b9ca511a7909
김동현 <gkfdkdlel@naver.com>
2023-09-26 11:55:34 +0900

    Refactor: FCM testpage

--- FrontEnd/src/components/Notification/FCMtestPage.jsx
+++ FrontEnd/src/components/Notification/FCMtestPage.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 import HeaderSub from '../Common/HeaderSub';
 import { customAxios } from '../api/customAxios';
+import { getToken, getMessaging } from 'firebase/messaging';
+import { initializeApp } from "firebase/app";
 
 const FCMtestPage = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAsVDuXDmuLEEmZGEzLXzC_JFOE9Dkv2yk",
+    authDomain: "gappa-5755f.firebaseapp.com",
+    projectId: "gappa-5755f",
+    storageBucket: "gappa-5755f.appspot.com",
+    messagingSenderId: "749380203321",
+    appId: "1:749380203321:web:3aba3212b3da7ac363aee8",
+    measurementId: "G-87Q4T0J8PG"
+  };
   const initialToken = localStorage.getItem("fcmToken");
   const [Fcmtoken] = useState(initialToken);
+  const app = initializeApp(firebaseConfig);
+  const messaging = getMessaging(app);
 
   const setting = () => {
     const body = {
@@ -21,13 +34,13 @@ const FCMtestPage = () => {
 
   async function subs() {
     console.log("권한 요청 중...");
-  
+
     const permission = await Notification.requestPermission();
     if (permission === "denied") {
       console.log("알림 권한이 허용되지 않음");
       return;
-    }       
-      
+    }
+
     console.log("알림 권한이 허용됨");
 
   }
@@ -43,6 +56,27 @@ const FCMtestPage = () => {
     })
   }
 
+  async function getFirebaseToken() {
+    try {
+      const token = await getToken(messaging, {
+        vapidKey: "BEGTfGO_ZzNesa6dGfyqdv6bLEy96rCBOcsZPV36Glm4MdYmWqVBhDjxIywtut1qVKq7hD_973Q3_fseOCFhuKU",
+      });
+
+      if (token) {
+        console.log("token: ", token);
+        localStorage.setItem("fcmToken", token);
+        alert("토큰을 성공적으로 가져왔습니다!");
+        return token;
+      } else {
+        console.log("Can not get Token");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error getting token: ", error);
+      return null;
+    }
+  }
+
   return (
     <div>
       <HeaderSub title={"FCM"} />
@@ -50,6 +84,7 @@ const FCMtestPage = () => {
       <div>Fcmtoken: {Fcmtoken}</div>
       <button onClick={setting }>fcm토큰보내기</button>
       <button onClick={subs }>알림 권한 허용 요청</button>
+      <button onClick={getFirebaseToken }>Firebase 토큰 가져오기</button>
     </div>
   );
 };
