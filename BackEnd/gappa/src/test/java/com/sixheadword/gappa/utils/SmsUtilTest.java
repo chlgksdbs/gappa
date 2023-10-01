3872eb3ea8e13caa7f5948692e009bb4b7f777db
김동현 <gkfdkdlel@naver.com>
2023-10-01 16:00:35 +0900

    feat: 예약 문자메시지 전송

--- BackEnd/gappa/src/test/java/com/sixheadword/gappa/utils/SmsUtilTest.java
+++ BackEnd/gappa/src/test/java/com/sixheadword/gappa/utils/SmsUtilTest.java
@@ -1,19 +1,25 @@
 package com.sixheadword.gappa.utils;
 
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 class SmsUtilTest {
 
-    @Autowired SmsUtil smsUtil;
-
+//    @Autowired SmsUtil smsUtil;
+//
 //    @Test
 //    public void 문자전송테스트() throws Exception {
-//        smsUtil.sendSMS("01024126237", "하이요");
+//        smsUtil.sendSMS("01073877808", "하이요");
+//        smsUtil.sendSMS("01073877808", "예약메시지", Optional.of(LocalDateTime.now().plusMinutes(11)));
 //    }
 }
