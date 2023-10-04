19eb38167f16ffaf9f288fbe9dbe60ed06d71504
DongIkkk <w8h0412@gmail.com>
2023-10-04 15:12:43 +0900

    Feat: 대출 요청과 응답 인증서 사용 적용

--- FrontEnd/src/components/Loan/Lend/LendSendPage.jsx
+++ FrontEnd/src/components/Loan/Lend/LendSendPage.jsx
@@ -3,15 +3,14 @@ import style from './LendSendPage.module.css';
 import { useNavigate, useLocation } from 'react-router-dom';
 import HeaderSub from '../../Common/HeaderSub';
 import { customAxios } from '../../api/customAxios';
-import PinPasswordVerify from '../../Auth/PinPasswordVerify';
+import toast, { Toaster } from 'react-hot-toast';
+import Keyboard from '../../Auth/Keyboard';
 
 const LendSendPage = () => {
   const navigate = useNavigate();
 
   const location = useLocation();
   const loanSeq = location.state.loanSeq;
-
-  const [showPinPasswordVerify, setShowPinPasswordVerify] = useState(false);
 
   const [toUser, setToUser] = useState("");
   const [fromUser, setFromUser] = useState("");
@@ -22,9 +21,29 @@ const LendSendPage = () => {
   const [yourAccountNumber, setYourAccountNumber] = useState("");
   const [yourBank, setYourBank] = useState("");
 
-  const handleSendButtonClick = () => {
-    setShowPinPasswordVerify(true);
+  const [isCertOpen, setIsCertOpen] = useState(false);
+  const [isCertSelect, setIsCertSelect] = useState(false);
+  const [imReady, setImReady] = useState(false);
+  const [certPW, setCertPW] = useState("");
+  const [modalCheck, setModalCheck] = useState(false);
+  const [check, setCheck] = useState("");
+
+  const [certName, setCertName] = useState("");
+  const expire = localStorage.getItem("expire");
+
+  const onClickPassword = (e) => {
+    setModalCheck(true);
+    setCheck(e);
   }
+
+  const inputHandler = (inputValue) => {
+    setCertPW(inputValue);
+  }
+
+  const modalHandelr = (inputValue) => {
+    setModalCheck(inputValue);
+  }
+
 
   useEffect(() => {
     getApply();
@@ -59,6 +78,14 @@ const LendSendPage = () => {
     });
   }
 
+  const selectCert = () => {
+    setIsCertSelect(true);
+  }
+
+  const toggleCert = () => {
+    setIsCertOpen(!isCertOpen);
+  }
+
   const lendStart = () => {
     const body = {
       loanSeq : loanSeq
@@ -81,24 +108,122 @@ const LendSendPage = () => {
     return formattedDate;
   }
 
-  const returnPinVerify = (status) => {
-    if(status === true) {
-      lendStart();
-    }
+  const formattDate = (date) => {
+    const dateObject = new Date(date);
+    const year = dateObject.getFullYear();
+    const month = dateObject.getMonth() + 1;
+    const day = dateObject.getDate();
+    const formattedDate = `${year}.${month < 10 ? ' 0' : ' '}${month}.${day < 10 ? ' 0' : ' '}${day}`;
+    return formattedDate;
   }
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      var base64Url = token.split(".")[1];
+      var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      var jwtPayload = decodeURIComponent(
+        atob(base64)
+          .split("")
+          .map(function (c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join("")
+      );
+      const JsonPayload = JSON.parse(jwtPayload);
+      const userSeq = JsonPayload.userSeq;
+    
+      customAxios.get(`/users/${userSeq}`)
+      .then((res)=>{
+        setCertName(res.data.data.name);
+      })
+      .catch((res)=>{
+        console.log(res);
+      })
+    } else {
+      // 토큰이 없는 경우 처리
+    }
+  }, []);
+
+  const complete = () => {
+    const body = {
+      pw: certPW,
+      public_key : localStorage.getItem("CPK")
+    }
+    customAxios.post("/certificate/encode", body)
+    .then((res)=>{
+      console.log(res);
+      const encryptedKey = res.data.encrypted
+
+      customAxios.post("/certificate/validate", {pw:encryptedKey})
+      .then((res)=>{
+        toast.success("인증되었습니다.", {
+          duration: 1000,
+        });
+        setTimeout(() => {
+          setIsCertSelect(false);
+          setIsCertOpen(false);
+          setImReady(true);
+        }, 1000);
+      })
+      .catch((res)=>{
+        toast.error("비밀번호가 틀렸습니다.", {
+          duration: 1000,
+        });
+        setTimeout(() => {
+          setCertPW("");
+        }, 1000);
+      })
+    })
+    .catch((res)=>{
+      console.log(res);
+    })
+  }
+
+
   return (
-    <div>
-      {showPinPasswordVerify ? (
-        <PinPasswordVerify result={returnPinVerify} />
-      ) : (
-      <div className={style.body}>
-        <HeaderSub title={"송금"} />
-        <div className={style.accountInfoBox}>
-          <div className={style.account}>{yourBank + " " +yourAccountNumber}</div>
-          <div className={style.money}>{principal.toLocaleString()} 원</div>
-          <div className={style.holder}>예금주 : {fromUser}</div>
+    <div className={style.body}>
+      <HeaderSub title={"송금"} />
+      <div><Toaster /></div>
+      { isCertSelect ? (
+        <div className={style.containerPW}>
+          <div className={style.mycert}>
+            <div className={style.subImgBox}>
+              <img src="/images/GapPassCert.png" alt="" />
+            </div>
+            <div className={style.certlogoBox}>
+              <img src="/images/GAPPASS.png" alt="" />
+            </div>
+            <div className={style.textBox}>
+              <div className={style.name}>{certName}</div>
+              <div className={style.detailcert}>이 기기에 인증서 보관중</div>
+              <div className={style.detailcert}>{formattDate(expire)}</div>
+            </div>
+          </div>
+          <div className={style.explanation}>
+            인증서 비밀번호를 입력하세요
+          </div>
+          <div className={style.passwordBox}>
+            <div>
+              비밀번호
+            </div>
+            <div>
+              <input type="password" className={style.input} name="password" value={certPW} onClick={() => onClickPassword("Pw")} readOnly />
+            </div>
+          </div>
+          <div className={style.btnBoxCert}>
+              <div onClick={() => complete()}>완료</div>
+          </div>
+          {
+            modalCheck && 
+            <Keyboard inputHandler={inputHandler} modalCheck={modalCheck} modalHandelr={modalHandelr}/> 
+          }
         </div>
+      ) : (
+      <div className={style.accountInfoBox}>
+        <div className={style.account}>{yourBank + " " +yourAccountNumber}</div>
+        <div className={style.money}>{principal.toLocaleString()} 원</div>
+        <div className={style.holder}>예금주 : {fromUser}</div>
         <div className={style.detail}>
           <div className={style.detailKey}>대여원금</div>
           <div className={style.detailValue}>{principal.toLocaleString()} 원</div>
@@ -128,11 +253,38 @@ const LendSendPage = () => {
           <div className={style.detailValue}>{toUser}</div>
         </div>
         <div className={style.btnBox}>
-          {/* <button onClick={()=> navigate('/lend/refuse')}>거절</button> */}
-          <button onClick={handleSendButtonClick}>송금</button>
+          { imReady ? (
+            <div className={style.completeBtn} onClick={lendStart}>
+              송금하기
+            </div>
+            ): (
+            <div className={style.nextBtn} onClick={toggleCert}>
+              서명하기
+            </div>
+          )}
+        </div>
+
+        <div className={`${style.cert} ${isCertOpen ? style.open : ''}`}>
+          <div className={style.certHeader}>
+            <p>인증서를 선택하세요.</p>
+            <p style={{marginTop: "5px"}} onClick={toggleCert}>✖</p>
+          </div>
+          <div className={style.mycert} onClick={()=> selectCert()}>
+            <div className={style.subImgBox}>
+              <img src="/images/GapPassCert.png" alt="" />
+            </div>
+            <div className={style.certlogoBox}>
+              <img src="/images/GAPPASS.png" alt="" />
+            </div>
+            <div className={style.textBox}>
+              <div className={style.name}>{certName}</div>
+              <div className={style.detailcert}>이 기기에 인증서 보관중</div>
+              <div className={style.detailcert}>{formattDate(expire)}</div>
+            </div>
+          </div>
         </div>
       </div>
-      )}
+      ) }
     </div>
   );
 };
