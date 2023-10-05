6bae09ea21b3f1e079ce9ecb1493debd65999472
김정훈 <junghun2581@naver.com>
2023-10-05 22:33:24 +0900

    Modify: 진짜 최종 css 수정

--- FrontEnd/src/components/Loan/Lend/LendSendPage.jsx
+++ FrontEnd/src/components/Loan/Lend/LendSendPage.jsx
@@ -195,9 +195,9 @@ const LendSendPage = () => {
             인증서 비밀번호를 입력하세요
           </div>
           <div className={style.passwordBox}>
-            <div>
+            {/* <div>
               비밀번호
-            </div>
+            </div> */}
             <div>
               <input type="password" className={style.input} name="password" value={certPW} onClick={() => onClickPassword("Pw")} readOnly />
             </div>
